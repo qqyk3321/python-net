60857575abec17b103884bf88e84bad271fb0915
qqyk3321 <taikongren0723@163.com>
2023-01-03 17:59:12 +0800

    telnet with ssh

--- telnet.py
+++ telnet.py
@@ -18,23 +18,37 @@ class ensp_telnet():
         out = self.tn.read_very_eager().decode('utf-8')
         # 回显处理，只保留查询到的结果，过滤掉查询输入字符
         return out
-class setup_ssh(ensp_telnet):
-    def __init__(self,host,port,eth_ip,eth_port,usrname,passwd,):
-        super().__init__(host,port)
-        self.eth_ip=eth_ip
-        self.eth_port=eth_port
-        self.usrname=usrname
-        self.passwd=passwd
-        self.cmd_list=['']
-    def write_cmd
+    def continuous_send(self,cmd_list):
+        for cmd in cmd_list:
+            self.send_cmd(cmd)
+
 
 if __name__ == "__main__":
 
     ip = "127.0.0.1"
-    port = 2000
-    tn=ensp_telnet()
+    port = 2002
+    tn=ensp_telnet(ip,port)
+    tn.send_cmd('system-view')
+    tn.send_cmd('interface GigabitEthernet 0/0/0')
+    tn.send_cmd('ip address 192.168.56.7 24')
+    tn.send_cmd('quit')
+    tn.send_cmd('stelnet server enable')
+    tn.send_cmd('rsa local-key-pair create')
+    tn.send_cmd('y')
+    tn.send_cmd('\n')
+    tn.send_cmd('aaa')
+    tn.send_cmd('local-user python password cipher qqyk3321')
+    tn.send_cmd('local-user python privilege level 15')
+    tn.send_cmd('local-user python service-type ssh')
+    tn.send_cmd('quit')
+    tn.send_cmd('user-interface vty 0 4')
+    tn.send_cmd('protocol inbound ssh')
+    tn.send_cmd('quit')
+    tn.send_cmd('ssh user python authentication-type all')
     
-    tn.read_until
+
+    out=tn.get_result()
+    print(out)
     #tn.write('at'.encode('utf-8') + b'\n')
 
     # time.sleep(1)
